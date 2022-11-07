ac90576f0cd7c58338fc387a941c387b3fc7b719
Fazzalis <117628490+Fazzalis@users.noreply.github.com>
2022-11-07 10:59:29 +0300

    Add files via upload

---
+++ ex_№24.pas
@@ -0,0 +1,15 @@
+﻿program pr24;
+var a,b,x:real;
+begin
+  writeln('введите коэффиценты a и b'); readln(a,b);
+  if a<>0
+  then begin
+    x:=(-1)*b/a; write('орень уравнения = ',x);
+   end
+  else
+  begin
+    if b<>0
+    then write('NO')
+    else write('INF');
+  end;
+end.
